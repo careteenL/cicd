c8360d5a54867156c7daf91d81d75e8365ec5e98
CareteenL <15074806497@163.com>
2021-02-19 10:44:25 +0800

    ci: 🎡 add gh-pages config

--- .umirc.ts
+++ .umirc.ts
@@ -1,7 +1,20 @@
 import { defineConfig } from 'dumi';
 
+const repo = 'cicd';
+const ghPagesBase = process.env.NODE_ENV === 'production' ? `/${repo}/` : '/';
+
 export default defineConfig({
-  title: 'Site Name',
+  title: `@careteen/${repo}`,
   mode: 'site',
+  hash: true,
+  publicPath: ghPagesBase,
+  base: ghPagesBase,
+  navs: [
+    null,
+    {
+      title: 'Github',
+      path: 'https://github.com/careteenL/cicd',
+    },
+  ],
   // more config: https://d.umijs.org/config
 });
